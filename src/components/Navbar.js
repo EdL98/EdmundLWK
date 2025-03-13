9da7f1d0bfd5953e9ff711a918befa0416def782
Edmund <edmundweekiat.low@digipen.edu>
2025-03-13 19:47:51 +0800

    test

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Navbar=()=>{
+    return (
+        <nav>
+            <a href="/">Portfolio</a>
+            <div>
+                <ul>
+                    
+                </ul>
+            </div>
+        </nav>
+    )
+}
